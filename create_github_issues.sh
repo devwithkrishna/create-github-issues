89612f9e7dec9cdce3b745d23b4a7633a2b59faa
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-28 14:55:38 +0530

    updated the script DEVOPS-26

--- create_github_issues.sh
+++ create_github_issues.sh
@@ -1,6 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 
-# GH_TOKEN --> will be passed from the workflow as an env variable
 ORGANIZATION=$1
 REPOSITORY_NAME=$2
 TITLE=$3
@@ -9,12 +8,27 @@ ASSIGNEES=$5
 MILESTONE=$6
 LABELS=$7
 
-# https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
+# Use an array for assignees and labels
+ASSIGNEES_ARRAY=($ASSIGNEES)
+LABELS_ARRAY=($LABELS)
 
+# Construct JSON data
+DATA=$(cat <<EOF
+{
+  "title": "$TITLE",
+  "body": "$BODY",
+  "assignees": ${ASSIGNEES:+[\"${ASSIGNEES_ARRAY[@]}\"]},
+  "milestone": "$MILESTONE",
+  "labels": ${LABELS:+[\"${LABELS_ARRAY[@]}\"]}
+}
+EOF
+)
+
+# Make the API call
 curl -L \
   -X POST \
-  -H "Accept: application/vnd.github+json" \
+  -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: Bearer $GH_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Content-Type: application/json" \
   https://api.github.com/repos/$ORGANIZATION/$REPOSITORY_NAME/issues \
-  -d "{\"title\":\"$TITLE\",\"body\":\"$BODY\",\"assignees\":[$ASSIGNEES],\"milestone\":\"$MILESTONE\",\"labels\":[$LABELS]}"
+  -d "$DATA"
