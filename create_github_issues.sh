f8f052b970a0e1aa4e8703cf526784faf2b720f8
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-28 15:01:15 +0530

    DEVOPS-26 script updated

--- create_github_issues.sh
+++ create_github_issues.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+/#!/bin/bash
 
 ORGANIZATION=$1
 REPOSITORY_NAME=$2
@@ -9,22 +9,25 @@ MILESTONE=$6
 LABELS=$7
 
 # Use an array for assignees and labels
+
 ASSIGNEES_ARRAY=($ASSIGNEES)
-LABELS_ARRAY=($LABELS)
+IFS=',' read -ra LABELS_ARRAY <<< "$LABELS"
 
 # Construct JSON data
+
 DATA=$(cat <<EOF
 {
   "title": "$TITLE",
   "body": "$BODY",
   "assignees": ${ASSIGNEES:+[\"${ASSIGNEES_ARRAY[@]}\"]},
   "milestone": "$MILESTONE",
-  "labels": ${LABELS:+[\"${LABELS_ARRAY[@]}\"]}
+  "labels": ${LABELS:+$(printf '"%s",' "${LABELS_ARRAY[@]}" | sed 's/,$//')}
 }
 EOF
 )
 
 # Make the API call
+
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
